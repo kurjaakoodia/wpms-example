73b5dd43b3908f18a24f6bd7afc4015eab6165df
kurjaakoodia <104067864+kurjaakoodia@users.noreply.github.com>
2023-08-25 11:34:29 +0300

    http-a

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,19 +1,15 @@
-import { TouchableOpacity, StyleSheet, View, Text, Image } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, Image } from "react-native";
 import PropTypes from "prop-types";
+import { mediaUrl } from "./utils/app-config";
 
-const ListItem = (props) => {
+const ListItem = (singleMedia) => {
   return (
-    <TouchableOpacity style={styles.someStyle}>
-      <View style={styles.someStyle}>
-        <Image
-          style={styles.someStyle}
-          source={{ uri: props.singleMedia.thumbnails.w160 }}
-        />
-      </View>
-      <View style={styles.someStyle}>
-        <Text style={styles.someStyle}>{props.singleMedia.title}</Text>
-        <Text>{props.singleMedia.description}</Text>
-      </View>
+    <TouchableOpacity style={styles.container}>
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{ uri: mediaUrl + singleMedia.thumbnails.w160 }}
+      />
+      <Text>{singleMedia.description}</Text>
     </TouchableOpacity>
   );
 };
