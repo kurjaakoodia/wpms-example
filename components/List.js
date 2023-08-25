830a6c4d9a8c34ba5bd18d751e0b0434e3a7fc3a
kurjaakoodia <104067864+kurjaakoodia@users.noreply.github.com>
2023-08-25 13:55:10 +0300

    http-b lisäyksiä

--- components/List.js
+++ components/List.js
@@ -1,35 +1,9 @@
 import { FlatList } from "react-native";
+import { useMedia } from "./hooks/apiHooks";
 import ListItem from "./ListItem";
-import { useEffect, useState } from "react";
-import { apiUrl } from "../utils/app-config";
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async () => {
-    try {
-      const response = await fetch(apiUrl + "media");
-      const json = await response.json();
-      // console.log(json);
-      const mediaFiles = await Promise.all(
-        json.map(async (item) => {
-          const response = await fetch(apiUrl + "media/" + item.file_id);
-          const fileData = await response.json();
-          // console.log('fileData', fileData);
-          return fileData;
-        }),
-      );
-      // console.log(data);
-      setMediaArray(mediaFiles);
-    } catch (error) {
-      console.error("loadMedia failed", error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia();
-  }, []);
-
+  const { mediaArray } = useMedia();
   return (
     <FlatList
       data={mediaArray}
