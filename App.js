30e1a86242ee2ac41faefafbe8d56c97ff95fc6a
kurjaakoodia <104067864+kurjaakoodia@users.noreply.github.com>
2023-08-27 13:39:45 +0300

    styles

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import {StatusBar} from 'expo-status-bar';
-import {Platform} from 'react-native';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import List from './components/List';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView>
       <List />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,22 +1,47 @@
-import {Image, Text, TouchableOpacity} from 'react-native';
+import {TouchableOpacity, StyleSheet, Text, Image, View} from 'react-native';
 import PropTypes from 'prop-types';
-
 const ListItem = ({singleMedia}) => {
   return (
     <TouchableOpacity
       onPress={() => {
-        console.log('touched!', singleMedia.title);
+        console.log('touched', singleMedia.title);
       }}
     >
-      <Image
-        style={{width: 100, height: 100}}
-        source={{uri: singleMedia.thumbnails.w160}}
-      />
-      <Text>{singleMedia.title}</Text>
-      <Text>{singleMedia.description}</Text>
+      <View style={styles.container}>
+        <Image
+          style={{borderRadius: 10, width: 100, height: 100}}
+          source={{uri: singleMedia.thumbnails.w160}}
+        />
+        <Text style={styles.text}>{singleMedia.title}</Text>
+        <Text style={styles.text}>{singleMedia.description}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexWrap: 'wrap',
+    flexGrow: 1,
+    marginTop: 30,
+    padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    flexWrap: 'wrap',
+    flexGrow: 1,
+    margin: 5,
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: '#000',
+    color: '#4CB7FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
