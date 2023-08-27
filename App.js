88253d65edfabc0620ea22f3b01eba4112a8d0f3
kurjaakoodia <104067864+kurjaakoodia@users.noreply.github.com>
2023-08-27 10:54:14 +0300

    first-app korjauksia

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import List from "./components/List";
-import { SafeAreaView } from "react-native";
-import ListItem from "./components/ListItem";
+import {StatusBar} from 'expo-status-bar';
+import {Platform} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import List from './components/List';
 
 const App = () => {
   return (
-    <>
-      <SafeAreaView style={ListItem.styles.container}>
-        <List />
-      </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <List />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,31 +1,22 @@
-import { TouchableOpacity, StyleSheet, View, Text, Image } from "react-native";
-import PropTypes from "prop-types";
+import {Image, Text, TouchableOpacity} from 'react-native';
+import PropTypes from 'prop-types';
 
-const ListItem = (props) => {
+const ListItem = ({singleMedia}) => {
   return (
-    <TouchableOpacity style={styles.someStyle}>
-      <View style={styles.someStyle}>
-        <Image
-          style={styles.someStyle}
-          source={{ uri: props.singleMedia.thumbnails.w160 }}
-        />
-      </View>
-      <View style={styles.someStyle}>
-        <Text style={styles.someStyle}>{props.singleMedia.title}</Text>
-        <Text>{props.singleMedia.description}</Text>
-      </View>
+    <TouchableOpacity
+      onPress={() => {
+        console.log('touched!', singleMedia.title);
+      }}
+    >
+      <Image
+        style={{width: 100, height: 100}}
+        source={{uri: singleMedia.thumbnails.w160}}
+      />
+      <Text>{singleMedia.title}</Text>
+      <Text>{singleMedia.description}</Text>
     </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
