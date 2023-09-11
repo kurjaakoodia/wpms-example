aa1019fbab956d6a4357600b3670e04bce5ea8c2
kurjaakoodia <104067864+kurjaakoodia@users.noreply.github.com>
2023-09-11 14:50:14 +0300

    uploadia lisää

--- utils/app-config.js
+++ utils/app-config.js
@@ -1,4 +1,5 @@
 const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
 const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+const placeholderImage = 'http://place-hold.it/200x200&text=Choose';
 
-export {apiUrl, mediaUrl};
+export {apiUrl, mediaUrl, placeholderImage};
--- views/Upload.js
+++ views/Upload.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card, Input, Button} from '@rneui/themed';
 import {Controller, useForm} from 'react-hook-form';
 import {ScrollView, StyleSheet} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import {placeholderImage} from '../utils/app-config';
 
 const Upload = () => {
+  const [image, setImage] = useState(placeholderImage);
   const {
     control,
     handleSubmit,
@@ -19,12 +22,27 @@ const Upload = () => {
     console.log('upload', uploadData);
   };
 
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <ScrollView>
       <Card>
         <Card.Image
-          source={{uri: 'https://placekitten.com/200/200'}}
+          source={{uri: image}}
           style={styles.image}
+          onPress={pickImage}
         />
         <Controller
           control={control}
