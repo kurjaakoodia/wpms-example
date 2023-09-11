f1500c22ff7cb1c4bcc55dbcb4bb218f944975d8
kurjaakoodia <104067864+kurjaakoodia@users.noreply.github.com>
2023-09-11 14:00:10 +0300

    uploadia lisää

--- views/Upload.js
+++ views/Upload.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card, Input, Button} from '@rneui/themed';
 import {Controller, useForm} from 'react-hook-form';
+import {ScrollView, StyleSheet} from 'react-native';
 
 const Upload = () => {
   const {
@@ -19,45 +20,59 @@ const Upload = () => {
   };
 
   return (
-    <Card>
-      <Controller
-        control={control}
-        rules={{
-          maxLength: 100,
-          required: {value: true, message: 'This is required.'},
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            placeholder="Title"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            errorMessage={errors.title?.message}
-          />
-        )}
-        name="title"
-      />
+    <ScrollView>
+      <Card>
+        <Card.Image
+          source={{uri: 'https://placekitten.com/200/200'}}
+          style={styles.image}
+        />
+        <Controller
+          control={control}
+          rules={{
+            maxLength: 100,
+            required: {value: true, message: 'This is required.'},
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              placeholder="Title"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              errorMessage={errors.title?.message}
+            />
+          )}
+          name="title"
+        />
 
-      <Controller
-        control={control}
-        rules={{
-          minLength: 10,
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            placeholder="Description (optional)"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            errorMessage={errors.description?.message}
-          />
-        )}
-        name="description"
-      />
-
-      <Button title="Upload" onPress={handleSubmit(upload)} />
-    </Card>
+        <Controller
+          control={control}
+          rules={{
+            minLength: {value: 10, message: 'min 10 characters'},
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              placeholder="Description (optional)"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              errorMessage={errors.description?.message}
+            />
+          )}
+          name="description"
+        />
+        <Button title="Upload" onPress={handleSubmit(upload)} />
+      </Card>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1,
+    marginBottom: 20,
+  },
+});
 
 export default Upload;
