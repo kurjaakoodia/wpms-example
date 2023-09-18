5b865a9f1a8bb81aba7674507ef48a19a9d686f9
kurjaakoodia <104067864+kurjaakoodia@users.noreply.github.com>
2023-09-18 15:14:00 +0300

    lisää ominaisuuksia, napit jne

--- views/Single.js
+++ views/Single.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Text} from 'react-native';
 import {mediaUrl} from '../utils/app-config';
@@ -6,11 +6,17 @@ import {Card, ListItem, Icon} from '@rneui/base';
 import {formatDate} from '../utils/functions';
 import {Video} from 'expo-av';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useUser} from '../components/hooks/apiHooks';
+import {useFavourite, useUser} from '../components/hooks/apiHooks';
+import {Button} from '@rneui/themed';
+import {MainContext} from '../contexts/MainContext';
 
 const Single = ({route, navigation}) => {
+  const {userLike, setUserLike} = useState(false);
   const [owner, setOwner] = useState({});
   const {getUserById} = useUser();
+  const {user} = useContext(MainContext);
+  const {likes, setLikes} = useState([]);
+  const {postFavourite, getFavouritesById, deleteFavourite} = useFavourite();
   const {
     title,
     description,
@@ -19,7 +25,42 @@ const Single = ({route, navigation}) => {
     user_id: userId,
     filesize,
     media_type: mediaType,
+    file_id: fileId,
   } = route.params;
+
+  const createFavourite = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const response = await postFavourite({file_id: fileId}, token);
+      response && setUserLike(true);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const removeFavourite = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const response = await deleteFavourite({fileId, token});
+      response && setUserLike(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const fetchLikes = async () => {
+    try {
+      const likesData = await getFavouritesById(fileId);
+      setLikes(likesData);
+      likesData.forEach((like) => {
+        if (like.user_id === user.user_id) {
+          setUserLike(true);
+        }
+      });
+    } catch (error) {
+      console.error('getFavourites', error.message);
+    }
+  };
 
   // fetch owner info
   const fetchOwner = async () => {
@@ -34,6 +75,10 @@ const Single = ({route, navigation}) => {
   useEffect(() => {
     fetchOwner();
   }, []);
+
+  useEffect(() => {
+    fetchLikes();
+  }, [userLike]);
 
   // Show full image and metadata
   return (
@@ -67,6 +112,14 @@ const Single = ({route, navigation}) => {
       <ListItem>
         <Icon name="person" />
         <Text>id: {owner.username}</Text>
+      </ListItem>
+      <ListItem>
+        {userLike ? (
+          <Button onPress={removeFavourite} title={'Dislike'} />
+        ) : (
+          <Button onPress={createFavourite} title={'Like'} />
+        )}
+        <Text>Total likes: {likes.length}</Text>
       </ListItem>
     </Card>
   );
